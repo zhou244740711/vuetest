5f57fe5b9a32d288266d0c04b3c45a7317e9f657
zhouzc <244740711@qq.com>
2021-03-26 17:01:55 +0800

    #

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import axios from 'axios'
+import router from './router'
+axios.defaults.timeout = 100000
+axios.defaults.withCredentials = true
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    devServer: {
+        host: '0.0.0.0',
+        port: 10000,
+        https: false,
+        hotOnly: false
+    }
+}
